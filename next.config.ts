1eb729917579e8eb39035c65185f69fefc7d406f
turt1e18 <pk0820@cu.ac.kr>
2025-02-24 17:21:33 +0900

    0224 add config redircet

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/main",
+        permanent: true
+      }
+    ];
+  }
 };
 
 export default nextConfig;
