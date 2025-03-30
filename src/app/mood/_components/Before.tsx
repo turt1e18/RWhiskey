e710dab9f62220d6dca77f234fc88c684c2f9820
turt1e18 <pk0820@cu.ac.kr>
2025-03-31 00:30:48 +0900

    mood build

---
+++ src/app/mood/_components/Before.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function BeforeScreen(props: any) {
+  const { setSwitchState } = props;
+  return (
+    <>
+      <></>
+    </>
+  );
+}
--- src/app/mood/page.tsx
+++ src/app/mood/page.tsx
@@ -1,8 +1,24 @@
 "use client";
+
+import { useState } from "react";
+import BeforeScreen from "./_components/Before";
+import AfterScreen from "./_components/After";
+
 export default function MoodScreen() {
-  return (
-    <>
-      <></>
-    </>
-  );
+  /**
+   * 0 데이터 세팅 화면
+   * 1 데이터 결과 화면
+   */
+  const [screenState, serScreenState] = useState(0);
+  const switchScreen = () => {
+    switch (screenState) {
+      case 0:
+        return <BeforeScreen setSwitchState={serScreenState} />;
+      case 1:
+        return <AfterScreen />;
+      default:
+        break;
+    }
+  };
+  return <>{switchScreen()}</>;
 }
